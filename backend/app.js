5630f171a4d5e11eee576486129ebd009658e4c9
ICT ACADEMY <thanvirmuhd@gmail.com>
2023-05-26 17:00:15 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,34 @@
-const express=require('express')// to build backend quickly
-const app=express();// insance of express
-const cors=require('cors');//for cross orgin policy
-app.use(cors());//means use thiscross package
-const PORT=7865
-app.use(express.json());// to parse json data
-app.use(express.urlencoded({extended:true}))//to parse url encoded data
-app.use(express.static('public'))//to serve static files
-require('./dB')//mongoose db connection folder is called here hen server starts up
+const express = require('express'); //! to build backend quickly 
+const app = express(); //!instance of express
+const cors = require('cors'); //!for cross orgin policy
+const PORT = 8745
+app.use(cors()); //! "app.use(cors())" means use this cors package when server starts up
+
+app.use(express.json()); //! to parse json data
+app.use(express.urlencoded({extended:true})); //! to parse url encoded data
+app.use(express.static('public')); //! to serve static files
+
+
+// DB 
+require('./dB') //!mongoose DB connection folder is called here hen server starts up
+const PRODUCT = require('./model/product')
+
+// To add product details coming from front end to backend database 
+app.post('/products',async (req,res)=>{
+    try {
+        console.log(req.body);
+        let item = req.body;
+
+        const saveItem = PRODUCT(item);
+        await saveItem.save();
+        res.send("Success!");
+
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
